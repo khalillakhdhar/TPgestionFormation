67784b04ee7e1a045e8599343e8cec6cc1008b78
khalil <khalillakhdharatc@gmail.com>
2022-03-04 12:05:54 +0100

    formations

--- src/java/BeansModel/Formation.java
+++ src/java/BeansModel/Formation.java
@@ -5,6 +5,13 @@
  */
 package BeansModel;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -19,7 +26,8 @@ public class Formation {
    private String description;
    private double prix;
    private String titre;
-
+Connection connection;
+    ArrayList<Formation> listFormations;
     public int getId() {
         return id;
     }
@@ -51,8 +59,80 @@ public class Formation {
     public void setTitre(String titre) {
         this.titre = titre;
     }
+    // scripts de connexion à la BD
+     public Connection getConnection(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");   //initialiser
+            connection = DriverManager.getConnection( "jdbc:mysql://localhost:3306/formation","root",""); //chaine(drive=jdbc:mysql, url="localhost:3306/base,username=root,password="")
+            System.out.println("connecté");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return connection;
+    }
+     
+      public List<Formation> showList()
+      {
+          try
+          {
+               listFormations=new ArrayList<Formation>();
+          //connection = getConnection();
+            Statement stmt=getConnection().createStatement();  
+            ResultSet rs=stmt.executeQuery("select * from formation"); 
+            while(rs.next()) //tant que rs à des valeurs
+            {
+                Formation f= new Formation();
+                f.setTitre(rs.getString("titre"));
+                f.setDescription(rs.getString("description"));
+                f.setPrix(rs.getDouble("prix"));
+                f.setId(rs.getInt("id"));
+                
+                listFormations.add(f);
+                
+            }
+      
+                  connection.close();        
+
+      
+          }
+          catch(Exception ex)
+          {
+          }
+                  //Formateur f2=new Formateur("nom2", "prenom2", "email2", "mdp2", "grade2", "tel2", 30, 2020);
+
+         // listFormateurs.add(f2);
+      
+      return listFormations;
+      
+      }
+      
+      public String addFormation()
+      {
+       connection = getConnection();
+         int result = 0;
+        try{
+            PreparedStatement stmt = connection.prepareStatement("INSERT INTO `formation` (`titre`, `description`, `prix`) VALUES (?,?,?)");
+            stmt.setString(1, this.titre);
+            stmt.setString(2, this.description);
+            stmt.setDouble(3, this.prix);
+           
+            result = stmt.executeUpdate();
+                        connection.close(); //optionelle
+
+            System.out.println("ajouté!");
+            
+         } catch(Exception e){
+            System.out.println(e);
+        }
+             if(result !=0)
+            return "formations.xhtml?faces-redirect=true";
+        else return "index.xhtml?faces-redirect=true";
     
-    
+      
+      
+      
+      
+      }
     
     
 }
